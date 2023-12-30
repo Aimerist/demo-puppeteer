e748902980bf117bbf412fd9d3d57ad3d0ed3c15
王佩馨 Aimer Wang <peihsing.wang@tpisoftware.com>
2023-12-30 17:41:18 +0800

    refactor: 拆分個步驟

--- app.js
+++ app.js
@@ -1,46 +1,71 @@
 const puppeteer = require("puppeteer");
 const fs = require("node:fs");
 const outputPath = "./Book";
+const bookUrl = "https://www.8book.com/novelbooks/194239/";
 
 (async () => {
   // 啟動瀏覽器並開啟一個新的空白頁面
+  const { page, browser } = await startBrowser();
+
+  // 取得章節清單
+  const chapters = await getChapterList(page, bookUrl);
+
+  for (let i = 0; i < 1; i++) {
+    const { title: chapterTitle, url: chapterUrl } = chapters[i];
+
+    // 取得文章內容，並去除雜訊
+    const text = await getArticle(page, chapterUrl);
+
+    // 儲存文章
+    saveArticle(chapterTitle, text);
+  }
+
+  // 關閉
+  await page.close();
+  await browser.close();
+})();
+
+// 啟動瀏覽器
+async function startBrowser() {
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: { width: 800, height: 1200 },
   });
   const page = await browser.newPage();
+  return { page, browser };
+}
 
-  // 將頁面導航至 URL
-  await page.goto("https://www.8book.com/novelbooks/194239/");
+// 取得章節目錄
+async function getChapterList(page, bookUrl) {
+  await page.goto(bookUrl);
 
-  // 取得章節資訊
   const chapters = await page.evaluate(() => {
     return Array.from(document.querySelectorAll(".episode_li")).map((dom) => ({
       title: dom.textContent.replace(/\n/g, "").trim(),
-      link: dom.href,
+      url: dom.href,
     }));
   });
+  return chapters;
+}
 
-  const chapter = chapters[0];
-
-  // 取得文章內容，並去除雜訊
-  await page.goto(chapter.link);
-  const textSelector = await page.waitForSelector("#text");
-  const text = await textSelector?.evaluate((el) =>
+// 取得文章
+async function getArticle(page, chapterUrl) {
+  await page.goto(chapterUrl);
+  const contentSelector = await page.waitForSelector("#text");
+  const content = await contentSelector?.evaluate((el) =>
     el.textContent
       .replace(/　　/g, "\r\n")
       .replace(/“/g, "「")
       .replace(/”/g, "」")
       .trim()
   );
+  return content;
+}
 
-  // 將文章內下儲存至txt
+// 儲存文章
+function saveArticle(title, article) {
   if (!fs.existsSync(outputPath)) {
     fs.mkdirSync(outputPath);
   }
-  fs.writeFileSync(`${outputPath}/${chapter.title}.txt`, text);
-
-  // 關閉
-  await page.close();
-  await browser.close();
-})();
+  fs.writeFileSync(`${outputPath}/${title}.txt`, article);
+}
