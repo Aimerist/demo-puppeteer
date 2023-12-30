74880ad81ed8790dca7a88d2bc7c3eb17ed53f14
王佩馨 Aimer Wang <peihsing.wang@tpisoftware.com>
2023-12-30 16:34:36 +0800

    feat: 將文章內容去除雜訊，並儲存至txt中

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const puppeteer = require("puppeteer");
+const fs = require("node:fs");
+const outputPath = "./Book";
 
 (async () => {
   // 啟動瀏覽器並開啟一個新的空白頁面
@@ -11,6 +13,7 @@ const puppeteer = require("puppeteer");
   // 將頁面導航至 URL
   await page.goto("https://www.8book.com/novelbooks/194239/");
 
+  // 取得章節資訊
   const chapters = await page.evaluate(() => {
     return Array.from(document.querySelectorAll(".episode_li")).map((dom) => ({
       title: dom.textContent.replace(/\n/g, "").trim(),
@@ -18,9 +21,26 @@ const puppeteer = require("puppeteer");
     }));
   });
 
-  await page.goto(chapters[0].link);
+  const chapter = chapters[0];
 
+  // 取得文章內容，並去除雜訊
+  await page.goto(chapter.link);
   const textSelector = await page.waitForSelector("#text");
-  const text = await textSelector?.evaluate((el) => el.textContent);
-  console.log(text);
+  const text = await textSelector?.evaluate((el) =>
+    el.textContent
+      .replace(/　　/g, "\r\n")
+      .replace(/“/g, "「")
+      .replace(/”/g, "」")
+      .trim()
+  );
+
+  // 將文章內下儲存至txt
+  if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath);
+  }
+  fs.writeFileSync(`${outputPath}/${chapter.title}.txt`, text);
+
+  // 關閉
+  await page.close();
+  await browser.close();
 })();
