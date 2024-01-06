30ec873699b42dc4eb3be394ea00529836b56ed4
王佩馨 Aimer Wang <peihsing.wang@tpisoftware.com>
2024-01-06 18:26:26 +0800

    feat: 檢查上次進度並接續下載(斷點續傳)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
 const fs = require("node:fs");
-const outputPath = "./Book";
+const outputPath = "./Book"; // TODO: 重構時，改命名 folderPath
 const bookUrl = "https://www.8book.com/novelbooks/194239/";
 
 (async () => {
@@ -9,8 +9,38 @@ const bookUrl = "https://www.8book.com/novelbooks/194239/";
 
   // 取得章節清單
   const chapters = await getChapterList(page, bookUrl);
+  let chapterBeginNum = 0;
+  const chapterEndNum = chapters.length;
 
-  for (let i = 0; i < 12; i++) {
+  // 1. 判斷是否有 Book 資料夾
+  if (fs.existsSync(outputPath)) {
+    // 2. 取得資料夾內所有檔案的名稱
+    const files = fs.readdirSync(outputPath);
+
+    // 3. 針對檔案名稱，取得最大數的章節序號
+    const fileMaxNum = files
+      .map((filename) => {
+        const match = filename.match(/\d+/);
+        return match ? parseInt(match[0], 10) : 0;
+      })
+      .reduce((max, num) => Math.max(max, num), 0);
+    chapterBeginNum = fileMaxNum ? fileMaxNum - 1 : 0;
+
+    // 4. 刪除最大數的章節序號
+    const deleteFilePath = `${outputPath}/Chapter_${fileMaxNum}.txt`;
+    try {
+      fs.unlinkSync(deleteFilePath);
+      console.log(`刪除 ${deleteFilePath} 檔案`);
+    } catch (err) {
+      console.error(`查無 ${deleteFilePath} 檔案`);
+    }
+  } else {
+    fs.mkdirSync(outputPath);
+    console.log(`建立 Book 資料夾`);
+    chapterBeginNum = 0;
+  }
+
+  for (let i = chapterBeginNum; i < chapterEndNum; i++) {
     const { url: chapterUrl } = chapters[i];
     const chapterTitle = `Chapter_${i + 1}`;
 
